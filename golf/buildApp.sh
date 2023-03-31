6b097beb329b4c67349c49f2b293966fb89cbc1c
Your Name <you@example.com>
2023-03-31 16:16:14 -0500

    FUCK

--- golf/buildApp.sh
+++ golf/buildApp.sh
@@ -5,7 +5,7 @@ function runApp() {
     echo "runApp() called."
 }
 
-if [ "$pwd" == "/home/ubuntu/flask/golf" ]; then
+if [ "$pwd" = "/home/ubuntu/flask/golf" ]; then
     echo "Correct directory, running application"
     runApp
 fi

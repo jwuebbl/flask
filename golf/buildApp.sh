a93d639aed09903b408970951dbd3db9cbe06be7
Your Name <you@example.com>
2023-03-31 15:55:04 -0500

    GSDF

--- golf/buildApp.sh
+++ golf/buildApp.sh
@@ -1,4 +1,4 @@
-if [(pwd) != /c/Users/JeffW/Desktop/flask/golf || (pwd) != /home/ubuntu/flask/golf]; then
+if [${pwd} != /c/Users/JeffW/Desktop/flask/golf || ${pwd} != /home/ubuntu/flask/golf]; then
     echo "You can't run this here"
     exit
 fi

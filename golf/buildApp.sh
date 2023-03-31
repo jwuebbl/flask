84b87e481de5c55e16e3c55024a8d3e6d83016de
Your Name <you@example.com>
2023-03-31 16:11:54 -0500

    FUCK

--- golf/buildApp.sh
+++ golf/buildApp.sh
@@ -1,10 +1,10 @@
 pwd = $(pwd)
 echo $pwd
 
-if [ $(pwd) != "/c/Users/JeffW/Desktop/flask/golf" ]; then
+if [ $pwd != "/c/Users/JeffW/Desktop/flask/golf" ]; then
   echo "You can't run this here"
   exit
-elif [ $(pwd) != "/home/ubuntu/flask/golf" ]; then
+elif [ $pwd != "/home/ubuntu/flask/golf" ]; then
     echo "You can't run this here"
     exit
 else

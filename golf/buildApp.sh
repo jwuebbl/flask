2f0774475d1748788ff0d3c34a2f6b473dd1996a
Your Name <you@example.com>
2023-03-31 16:20:34 -0500

    FUCK

--- golf/buildApp.sh
+++ golf/buildApp.sh
@@ -6,6 +6,9 @@ if [ "$(pwd)" = "/home/ubuntu/flask/golf" ]; then
     echo "Correct directory, running application"
     runApp
 elif [ "$(pwd)" = "/home/ubuntu/flask" ]; then
-    cd ./golf
-    echo $(pwd)
+    echo "buildApp.sh called from $(pwd)."
+    runApp
+else
+    echo "Can't run the script from this directory"
+    exit
 fi
